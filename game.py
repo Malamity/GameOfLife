46204fdcacb8c763f115fa95e2de04df85bfbac7
Michał Szura <115370163+Malamity@users.noreply.github.com>
2023-12-11 20:18:13 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,90 +1,177 @@
-import pygame
-import numpy as np
+import tkinter as tk
+import random
+import pickle
+from tkinter import messagebox
+from tkinter import filedialog
 
-# Initialize Pygame
-pygame.init()
+from abc import ABC, abstractmethod
 
-# Screen dimensions
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
 
-# Grid dimensions
-n_cells_x, n_cells_y = 40, 30
-cell_width = width // n_cells_x
-cell_height = height // n_cells_y
+class AbstractCell(ABC):
+    def __init__(self, is_alive=False):
+        self.is_alive = is_alive
 
-# Game state
-game_state = np.random.choice([0, 1], size=(n_cells_x, n_cells_y), p=[0.8, 0.2])
+    @abstractmethod
+    def update(self, is_alive):
+        pass
 
-# Colors
-white = (255, 255, 255)
-black = (0, 0, 0)
-gray = (128, 128, 128)
-green = (0, 255, 0)
 
-# Button dimensions
-button_width, button_height = 200, 50
-button_x, button_y = (width - button_width) // 2, height - button_height - 10
+class AbstractCellFactory(ABC):
+    @abstractmethod
+    def create_cell(self, is_alive=False):
+        pass
 
-def draw_button():
-    pygame.draw.rect(screen, green, (button_x, button_y, button_width, button_height))
-    font = pygame.font.Font(None, 36)
-    text = font.render("Next Generation", True, black)
-    text_rect = text.get_rect(center=(button_x + button_width // 2, button_y + button_height // 2))
-    screen.blit(text, text_rect)
 
-def draw_grid():
-    for y in range(0, height, cell_height):
-        for x in range(0, width, cell_width):
-            cell = pygame.Rect(x, y, cell_width, cell_height)
-            pygame.draw.rect(screen, gray, cell, 1)
+class Cell(AbstractCell):
+    def update(self, is_alive):
+        self.is_alive = is_alive
 
-def next_generation():
-    global game_state
-    new_state = np.copy(game_state)
 
-    for y in range(n_cells_y):
-        for x in range(n_cells_x):
-            n_neighbors = game_state[(x - 1) % n_cells_x, (y - 1) % n_cells_y] + \
-                          game_state[(x)     % n_cells_x, (y - 1) % n_cells_y] + \
-                          game_state[(x + 1) % n_cells_x, (y - 1) % n_cells_y] + \
-                          game_state[(x - 1) % n_cells_x, (y)     % n_cells_y] + \
-                          game_state[(x + 1) % n_cells_x, (y)     % n_cells_y] + \
-                          game_state[(x - 1) % n_cells_x, (y + 1) % n_cells_y] + \
-                          game_state[(x)     % n_cells_x, (y + 1) % n_cells_y] + \
-                          game_state[(x + 1) % n_cells_x, (y + 1) % n_cells_y]
+class CellFactory(AbstractCellFactory):
+    def create_cell(self, is_alive=False):
+        return Cell(is_alive)
 
-            if game_state[x, y] == 1 and (n_neighbors < 2 or n_neighbors > 3):
-                new_state[x, y] = 0
-            elif game_state[x, y] == 0 and n_neighbors == 3:
-                new_state[x, y] = 1
 
-    game_state = new_state
+class GameOfLife:
+    def __init__(self, rows, cols, cell_factory):
+        self.rows = rows
+        self.cols = cols
+        self.grid = [[cell_factory.create_cell() for _ in range(cols)]
+                     for _ in range(rows)]
 
-def draw_cells():
-    for y in range(n_cells_y):
-        for x in range(n_cells_x):
-            cell = pygame.Rect(x * cell_width, y * cell_height, cell_width, cell_height)
-            if game_state[x, y] == 1:
-                pygame.draw.rect(screen, black, cell)
+    def step(self):
+        changes = []
+        for i in range(self.rows):
+            for j in range(self.cols):
+                live_neighbours = self.count_live_neighbours(i, j)
+                if self.grid[i][j].is_alive:
+                    if live_neighbours < 2 or live_neighbours > 3:
+                        changes.append((i, j, False))
+                else:
+                    if live_neighbours == 3:
+                        changes.append((i, j, True))
 
-running = True
-while running:
-    screen.fill(white)
-    draw_grid()
-    draw_cells()
-    draw_button()
-    pygame.display.flip()
+        for i, j, state in changes:
+            self.grid[i][j].update(state)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if button_x <= event.pos[0] <= button_x + button_width and button_y <= event.pos[1] <= button_y + button_height:
-                next_generation()
-            else:
-                x, y = event.pos[0] // cell_width, event.pos[1] // cell_height
-                game_state[x, y] = not game_state[x, y]
+    def count_live_neighbours(self, row, col):
+        directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
+                      (0, 1), (1, -1), (1, 0), (1, 1)]
+        live_neighbours = 0
+        for dr, dc in directions:
+            r, c = row + dr, col + dc
+            if 0 <= r < self.rows and 0 <= c < self.cols and self.grid[r][c].is_alive:
+                live_neighbours += 1
+        return live_neighbours
 
-pygame.quit()
+    def randomize(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.grid[i][j].is_alive = random.choice([True, False])
 
+
+class GameOfLifeGUI:
+    def __init__(self, root, rows, cols, cell_size=10):
+        # GUI initialization
+        self.root = root
+        self.rows = rows
+        self.cols = cols
+        self.cell_size = cell_size
+        self.game_running = False
+        self.game = GameOfLife(rows, cols, CellFactory())
+        self.canvas = tk.Canvas(
+            root, width=cols * cell_size, height=rows * cell_size)
+        self.canvas.pack()
+        self.start_button = tk.Button(
+            root, text="Start", command=self.toggle_game_state)
+        self.start_button.pack()
+        self.draw_grid()
+        self.game.randomize()
+        self.update_canvas()
+
+        # Save and Load buttons
+        self.save_button = tk.Button(root, text="Save", command=self.save_game)
+        self.save_button.pack()
+        self.load_button = tk.Button(root, text="Load", command=self.load_game)
+        self.load_button.pack()
+
+    def toggle_game_state(self):
+        if self.game_running:
+            self.game_running = False
+            self.start_button.config(text="Start")
+        else:
+            self.game_running = True
+            self.start_button.config(text="Stop")
+            self.run_game()
+
+    def run_game(self):
+        if self.game_running:
+            self.game.step()
+            self.update_canvas()
+            self.root.after(100, self.run_game)
+
+    def draw_grid(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                x1 = j * self.cell_size
+                y1 = i * self.cell_size
+                x2 = x1 + self.cell_size
+                y2 = y1 + self.cell_size
+                self.canvas.create_rectangle(
+                    x1, y1, x2, y2, fill="white", outline="gray", tags=f"cell_{i}_{j}")
+                self.canvas.tag_bind(
+                    f"cell_{i}_{j}", '<Button-1>', lambda event, i=i, j=j: self.toggle_cell(i, j))
+
+    def update_canvas(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                color = "black" if self.game.grid[i][j].is_alive else "white"
+                self.canvas.itemconfig(f"cell_{i}_{j}", fill=color)
+
+    def toggle_cell(self, row, col):
+        cell = self.game.grid[row][col]
+        cell.update(not cell.is_alive)
+        self.update_canvas()
+
+    def save_game(self):
+        filepath = filedialog.asksaveasfilename(
+            defaultextension=".gol",
+            filetypes=[("Game of Life files", "*.gol"), ("All files", "*.*")],
+        )
+        if not filepath:
+            return
+
+        try:
+            with open(filepath, "wb") as file:
+                pickle.dump([[cell.is_alive for cell in row]
+                            for row in self.game.grid], file)
+            messagebox.showinfo(
+                "Save Game", "The game was successfully saved!")
+        except Exception as e:
+            messagebox.showerror("Save Game", f"An error occurred: {e}")
+
+    def load_game(self):
+        filepath = filedialog.askopenfilename(
+            filetypes=[("Game of Life files", "*.gol"), ("All files", "*.*")]
+        )
+        if not filepath:
+            return
+
+        try:
+            with open(filepath, "rb") as file:
+                grid_state = pickle.load(file)
+
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    self.game.grid[i][j].update(grid_state[i][j])
+            self.update_canvas()
+            messagebox.showinfo(
+                "Load Game", "The game was successfully loaded!")
+        except Exception as e:
+            messagebox.showerror("Load Game", f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = GameOfLifeGUI(root, 50, 50, 10)
+    root.mainloop()
